7d6ef7f9139ab1aab9c36f7f497d74c3ccfa0754
hamzahdiza <hamzah.diza@gmail.com>
2024-02-12 01:56:05 +0700

    Delete contact

--- src/main/java/diza/restful/service/ContactService.java
+++ src/main/java/diza/restful/service/ContactService.java
@@ -74,6 +74,14 @@ public class ContactService {
         return toContactResponse(contact);
     }
 
+    @Transactional
+    public void delete(User user, String contactId) {
+        Contact contact = contactRepository.findFirstByUserAndId(user, contactId)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Contact not found"));
+
+        contactRepository.delete(contact);
+    }
+
 
     private ContactResponse toContactResponse(Contact contact) {
         return ContactResponse.builder()
